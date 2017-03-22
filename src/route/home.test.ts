d1bbd3cd3c2fd32087dcb95ae886c8cee24502ca
Craig Russell <craig@craig-russell.co.uk>
2017-03-22 21:43:32 +0000

    Reformatted Basic API Route tests

--- src/route/home.test.ts
+++ src/route/home.test.ts
@@ -4,41 +4,36 @@ import { getTestApp } from "../bootstrap";
 
 let app: SuperTest<Test>;
 
-describe("GET /", () => {
+describe("Basic API routes", () => {
 
     before(done => {
         getTestApp().then(a => { app = a; done(); });
     });
 
-    it("should return a friendly JSON message", done => {
-        app.get("/")
-            .expect(200)
-            .end((err, res) => err ? done(err) : done());
-    });
-});
+    describe("GET /", () => {
 
-describe("GET /teapot", () => {
-
-    before(done => {
-        getTestApp().then(a => { app = a; done(); });
+        it("should return a friendly JSON message", done => {
+            app.get("/")
+                .expect(200)
+                .end((err, res) => err ? done(err) : done());
+        });
     });
 
-    it("should return 418 `I'm A Teapot`", done => {
-        app.get("/teapot")
-            .expect(418)
-            .end((err, res) => err ? done(err) : done());
-    });
-});
+    describe("GET /teapot", () => {
 
-describe("GET /error", () => {
-
-    before(done => {
-        getTestApp().then(a => { app = a; done(); });
+        it("should return 418 `I'm A Teapot`", done => {
+            app.get("/teapot")
+                .expect(418)
+                .end((err, res) => err ? done(err) : done());
+        });
     });
 
-    it("should return 500 `Internal Server Error`", done => {
-        app.get("/error")
-            .expect(500)
-            .end((err, res) => err ? done(err) : done());
+    describe("GET /error", () => {
+
+        it("should return 500 `Internal Server Error`", done => {
+            app.get("/error")
+                .expect(500)
+                .end((err, res) => err ? done(err) : done());
+        });
     });
 });
