7cb3b7a5a112f3649449452c5c2bc0929877f0bc
Craig Russell <craig@craig-russell.co.uk>
2017-03-22 20:47:58 +0000

    Added test template for Products API

--- src/route/products.test.ts
+++ src/route/products.test.ts
@@ -2,15 +2,85 @@ import "mocha";
 import { SuperTest, Test } from "supertest";
 import { getTestApp } from "../bootstrap";
 
-describe("GET /products", () => {
+describe("Products API", () => {
+
     let app: SuperTest<Test>;
 
     before(done => {
         getTestApp().then(a => { app = a; done(); });
     });
 
-    it("should return all products", () => {
-        app.get("/products")
-            .expect(200);
+    xdescribe("GET /products", () => {
+
+        it("should create and return a new product", done => {
+            app.get("/products")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    xdescribe("POST /products", () => {
+
+        it("should create and return a new product", done => {
+            app.get("/products")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    xdescribe("GET /products/:id", () => {
+
+        it("should 404 if no Product exists with `id`", done => {
+            app.get("/products/999")
+                .expect("Content-Type", /json/)
+                .expect(404, { error: { message: "Not Found" } })
+                .end((err, res) => err ? done(err) : done());
+
+        });
+
+        it("should get product with `id`", done => {
+            app.get("/products")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    xdescribe("POST /products/:id", () => {
+
+        it("should 404 if no Product exists with `id`", done => {
+            app.get("/products/999")
+                .expect("Content-Type", /json/)
+                .expect(404, { error: { message: "Not Found" } })
+                .end((err, res) => err ? done(err) : done());
+
+        });
+
+        it("should update product with `id`", done => {
+            app.get("/products")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    xdescribe("DELETE /products/:id", () => {
+
+        it("should 404 if no Product exists with `id`", done => {
+            app.get("/products/999")
+                .expect("Content-Type", /json/)
+                .expect(404, { error: { message: "Not Found" } })
+                .end((err, res) => err ? done(err) : done());
+
+        });
+
+        it("should delete product with `id`", done => {
+            app.get("/products")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => err ? done(err) : done());
+        });
     });
 });
