b520b7b587b96681a51f75d55646cd728828cb72
Craig Russell <craig@craig-russell.co.uk>
2017-03-22 21:43:49 +0000

    Built out Product API tests

--- src/route/products.test.ts
+++ src/route/products.test.ts
@@ -1,6 +1,8 @@
 import "mocha";
 import { SuperTest, Test } from "supertest";
 import { getTestApp } from "../bootstrap";
+import { Products } from "../controller/Products";
+import { Product } from "../entity/Product";
 
 describe("Products API", () => {
 
@@ -10,77 +12,139 @@ describe("Products API", () => {
         getTestApp().then(a => { app = a; done(); });
     });
 
-    xdescribe("GET /products", () => {
 
-        it("should create and return a new product", done => {
+    describe("GET /products", () => {
+
+        before(done => {
+            Promise.all([
+                Products.add({ name: "Apples" }),
+                Products.add({ name: "Oranges" }),
+                Products.add({ name: "Pears" }),
+            ]).then(() => done());
+        });
+
+        after(done => {
+            Products.deleteAll().then(() => done());
+        });
+
+        it("should return all Products", done => {
             app.get("/products")
+                .expect(res => {
+                    if (!res.body.data) throw new Error("Expected data in response");
+                    if (res.body.data.length != 3) throw new Error("Expected 3 Products in response");
+                })
                 .expect("Content-Type", /json/)
-                .expect(200)
-                .end((err, res) => err ? done(err) : done());
+                .expect(200, done);
         });
     });
 
-    xdescribe("POST /products", () => {
+    describe("POST /products", () => {
+
+        let fields = { name: "Bananas" };
+
+        after(done => {
+            Products.deleteAll().then(() => done());
+        });
 
         it("should create and return a new product", done => {
-            app.get("/products")
+            app.post("/products")
+                .send(fields)
+                .expect(res => {
+                    if (!res.body.data) throw new Error("Expected data in response");
+                    if (!res.body.data.id) throw new Error("Expected Product to have id");
+                    if (!res.body.data.name) throw new Error("Expected Product to have name");
+                    if (res.body.data.name !== fields.name) throw new Error(`Expected Product name to be '${fields.name}'`);
+                })
                 .expect("Content-Type", /json/)
-                .expect(200)
-                .end((err, res) => err ? done(err) : done());
+                .expect(200, done);
         });
     });
 
-    xdescribe("GET /products/:id", () => {
+    describe("GET /products/:id", () => {
+
+        let product: Product;
+
+        before(done => {
+            Products.add({ name: "Grapes" }).then(i => { product = i; done() });
+        });
+
+        after(done => {
+            Products.deleteAll().then(() => done());
+        });
 
         it("should 404 if no Product exists with `id`", done => {
-            app.get("/products/999")
+            app.get(`/products/9999`)
                 .expect("Content-Type", /json/)
-                .expect(404, { error: { message: "Not Found" } })
-                .end((err, res) => err ? done(err) : done());
-
+                .expect(404, { error: { message: "Not Found" } }, done);
         });
 
         it("should get product with `id`", done => {
-            app.get("/products")
+            app.get(`/products/${product.id}`)
                 .expect("Content-Type", /json/)
-                .expect(200)
-                .end((err, res) => err ? done(err) : done());
+                .expect(200, {
+                    data: {
+                        id: product.id,
+                        name: product.name
+                    }
+                }, done);
         });
     });
 
-    xdescribe("POST /products/:id", () => {
+    describe("POST /products/:id", () => {
+
+        let product: Product;
+        let fields = { name: "Mangos" };
+
+        before(done => {
+            Products.add({ name: "Strawberries" }).then(i => { product = i; done() });
+        });
+
+        after(done => {
+            Products.deleteAll().then(() => done());
+        });
 
         it("should 404 if no Product exists with `id`", done => {
-            app.get("/products/999")
+            app.post("/products/9999")
+                .send(fields)
                 .expect("Content-Type", /json/)
-                .expect(404, { error: { message: "Not Found" } })
-                .end((err, res) => err ? done(err) : done());
-
+                .expect(404, { error: { message: "Not Found" } }, done);
         });
 
         it("should update product with `id`", done => {
-            app.get("/products")
+            app.post(`/products/${product.id}`)
+                .send(fields)
                 .expect("Content-Type", /json/)
-                .expect(200)
-                .end((err, res) => err ? done(err) : done());
+                .expect(200, {
+                    data: {
+                        id: product.id,
+                        name: fields.name
+                    }
+                }, done);
         });
     });
 
-    xdescribe("DELETE /products/:id", () => {
+    describe("DELETE /products/:id", () => {
+
+        let product: Product;
+
+        before(done => {
+            Products.add({ name: "Strawberries" }).then(i => { product = i; done() });
+        });
+
+        after(done => {
+            Products.deleteAll().then(() => done());
+        });
 
         it("should 404 if no Product exists with `id`", done => {
-            app.get("/products/999")
+            app.delete("/products/9999")
                 .expect("Content-Type", /json/)
-                .expect(404, { error: { message: "Not Found" } })
-                .end((err, res) => err ? done(err) : done());
-
+                .expect(404, { error: { message: "Not Found" } }, done);
         });
 
         it("should delete product with `id`", done => {
-            app.get("/products")
+            app.delete(`/products/${product.id}`)
                 .expect("Content-Type", /json/)
-                .expect(200)
-                .end((err, res) => err ? done(err) : done());
+                .expect(200, { data: product.id }, done);
         });
     });
 });
