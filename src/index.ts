7b756dd1be88317dbfec73a88a41c3fb9f2dbe37
Craig Russell <craig.russell@cloudcall.com>
2017-03-23 15:03:10 +0000

    Support loading config from json file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import "reflect-metadata";
+import * as merge from "deepmerge";
+import { join } from "path";
 import { argv } from "yargs";
 import { ConnectionOptions } from "typeorm";
 import { Application, ApplicationOptions } from "./lib/application";
 
-let metadata = require("../package.json");
-
-// Get CLI args or defaults
-const HOST = argv.host || "127.0.0.1";
-const PORT = argv.port || 1337;
-const CONFIG = argv.config;
+let metadata = require(join("..","package.json"));
 
 // Default application options suitable for development environment
-// TODO Override values from JSON file provided on CLI 
 let options: ApplicationOptions = {
     database: {
         driver: {
@@ -30,8 +26,18 @@ let options: ApplicationOptions = {
     }
 };
 
+try {
+    // Override values from JSON file provided on CLI 
+    options = merge(options, require(join("..", argv.config)));
+} catch (e) { }
+
+// Get CLI args or defaults
+const HOST = argv.host || "127.0.0.1";
+const PORT = argv.port || 1337;
+
+// Start Application
 Application.getApp(options).then(app => {
     app.listen(PORT, HOST, () => {
         Application.logger.info(`Serving on http://${HOST}:${PORT}`);
     });
-})
+});
