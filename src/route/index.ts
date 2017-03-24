f114d009a9486c76f74dcf469e2b53b980f728c4
Craig Russell <craig.russell@cloudcall.com>
2017-03-24 09:48:21 +0000

    Renamed Router modules

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import * as home from "../route/home";
-import * as products from "../route/products";
+import * as homeRouter from "../route/homeRouter";
+import * as productRouter from "../route/productRouter";
 
 const router = Router();
 
 // Apply imported routes
 // TODO Autoload all route modules
-router.use(home);
-router.use(products);
+router.use(homeRouter);
+router.use(productRouter);
 
 export = router;
